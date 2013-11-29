f208cca7967180d897621a203db55e83041798e5
amashigeseiji <amashige@tejimaya.com>
2013-11-29 15:18:25 +0900

    不要なコメント行を削除

--- apps/api/modules/diaryComment/templates/postSuccess.php
+++ apps/api/modules/diaryComment/templates/postSuccess.php
@@ -2,7 +2,6 @@
 use_helper('opDiary');
 $data = op_api_diary_comment($comment);
 $data['deletable'] = $comment->isDeletable($memberId);
-//unset($data['images']);
 
 return array(
   'status' => 'success',
